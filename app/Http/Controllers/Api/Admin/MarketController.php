776a2512f997e7da7db77e22b9d9fe41bf82c4dc
Cindy <cindy@netblaze.net>
2025-08-10 05:55:00 +0800

    opt

--- app/Http/Controllers/Api/Admin/MarketController.php
+++ app/Http/Controllers/Api/Admin/MarketController.php
@@ -524,8 +524,8 @@ class MarketController extends ApiController {
                 'low'         => $targetLow,
                 'close'       => $close,
                 'sigma'       => 0.02,
-                'start_at'    => Carbon::parse($startTime)->toDateTimeString(),
-                'end_at'      => Carbon::parse($endTime)->toDateTimeString(),
+                'start_at'    => Carbon::parse($startTime)->setTimezone(config('app.timezone'))->toDateTimeString(),
+                'end_at'      => Carbon::parse($endTime)->setTimezone(config('app.timezone'))->toDateTimeString(),
             ];
             Cache::set($taskKey, json_encode($task), $ttl);
             $interval = config('kline.interval', "1m");
