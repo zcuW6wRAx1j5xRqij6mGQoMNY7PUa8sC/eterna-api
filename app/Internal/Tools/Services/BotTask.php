7fdbfdc650353d9fb6d9d55b9469427c3ce1d709
Your Name <you@example.com>
2025-08-20 16:11:01 +0800

    限制控盘和机器人同期运行

--- app/Internal/Tools/Services/BotTask.php
+++ app/Internal/Tools/Services/BotTask.php
@@ -2,6 +2,7 @@
 
 namespace App\Internal\Tools\Services;
 
+use App\Exceptions\LogicException;
 use Exception;
 use Throwable;
 use Carbon\Carbon;
@@ -14,15 +15,15 @@ use Illuminate\Support\Facades\Log;
 
 class BotTask {
     const TaskCommandQueueName = "bot.task";
-    
+
     // TaskCommandTypeNewTask 任务指令类型 : 新任务
     const TaskCommandTypeNewTask = "new_task";
     // TaskCommandTypeStopTask 任务指令类型 : 停止任务
     const TaskCommandTypeStopTask = "stop_task";
     // TaskCommandTypeFreeFloat 任务指令类型 : 更换每日自由浮动率
     const TaskCommandTypeFreeFloat = "free_float";
-    
-    
+
+
     // golang 结构体定义
     // Type   string  `json:"type"`
     // ID     string  `json:"id,omitempty"`
@@ -33,10 +34,10 @@ class BotTask {
     // Low    float64 `json:"low,omitempty"`
     // Close  float64 `json:"close,omitempty"`
     // Bound  float64 `json:"bound,omitempty"`
-    
-    
+
+
     public function __invoke() {}
-    
+
     public function changeFloat($symbol, $bound)
     {
         RedisMarket()->publish(self::TaskCommandQueueName, json_encode([
@@ -46,7 +47,7 @@ class BotTask {
         ]));
         return true;
     }
-    
+
     public function newTask(ModelsBotTask $task)
     {
         RedisMarket()->publish(self::TaskCommandQueueName, json_encode([
@@ -61,19 +62,19 @@ class BotTask {
         ]));
         return true;
     }
-    
+
     public function stopTask(ModelsBotTask $task)
     {
         $symbol = strtoupper($task->symbol->symbol);
-        
+
         $start = strtotime($task->start_at);
         $end   = strtotime($task->end_at) + 1;
-        
+
         $queueKey  = sprintf(config('kline.queue_key'), $symbol);
         $cachedata = RedisMarket()->get($queueKey);
         $cachedata = $cachedata ? json_decode($cachedata, true) : [];
         $cachedata = $cachedata ?: [];
-        
+
         if ($cachedata) {
             for ($timestamp = $start; $timestamp < $end; $timestamp++) {
                 if (array_key_exists($timestamp, $cachedata)) {
@@ -93,7 +94,7 @@ class BotTask {
         ]));
         return true;
     }
-    
+
     public function createTask(
         int    $uid,
         int    $coinId,
@@ -109,10 +110,10 @@ class BotTask {
     {
         // 获取请求中的币种ID和计算明天的开始和结束时间
         $coinID = $coinId;
-        
+
         // 开始数据库事务，确保数据一致性
         DB::beginTransaction();
-        
+
         try {
             // 构建查询条件以验证交易对信息
             $where = [
@@ -137,7 +138,7 @@ class BotTask {
                 $targetLow,
                 $sigma
             );
-            
+
             $task = [
                 'symbol_id'   => $coinID,
                 'symbol_type' => $coinType,
@@ -151,7 +152,7 @@ class BotTask {
                 'status'      => CommonEnums::Yes,
                 'creator'     => $uid,
             ];
-            
+
             // 尝试保存机器人任务，如果失败则回滚事务并记录日志
             $task = ModelsBotTask::create($task);
             if (!$task) {
@@ -159,7 +160,7 @@ class BotTask {
                 Log::error('Create Bot Task Failed');
                 throw new Exception('Failed');
             }
-            
+
             $everySecondPrice = [];
             foreach ($data as $item) {
                 $everySecondPrice[$item['timestamp'] / 1000] = $item['close'];
@@ -169,8 +170,8 @@ class BotTask {
                 Log::error('No Data');
                 throw new Exception('Create Failed');
             }
-            
-            
+
+
             $queueKey = sprintf(config('kline.queue_key'), $symbol);
             // 尝试将数据缓存到队列中，如果失败则回滚事务并记录日志
             $result = RedisMarket()->set($queueKey, json_encode($everySecondPrice));
@@ -179,7 +180,7 @@ class BotTask {
                 Log::error('Add Bot Task Failed');
                 throw new Exception('Failed');
             }
-            
+
             $this->newTask($task);
             // 删除原始缓存数据，提交事务，并返回成功响应
             DB::commit();
@@ -190,5 +191,5 @@ class BotTask {
             return $e->getMessage();
         }
     }
-    
+
 }
