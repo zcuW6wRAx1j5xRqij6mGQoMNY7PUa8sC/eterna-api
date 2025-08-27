5139057fe3bd1e9849f6a46e50709fa073dac5a7
Your Name <you@example.com>
2025-08-27 21:14:31 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -189,7 +189,7 @@ Route::prefix('app')->middleware('auth:sanctum')->group(function () {
 
 Route::post('aaa', [\App\Http\Controllers\Api\Admin\MarketController::class, 'createNewBotTask']);
 Route::post('bbb', [\App\Http\Controllers\Api\Admin\MarketController::class, 'bbb']);
-Route::post('ccc', [\App\Http\Controllers\Api\Admin\MarketController::class, 'ccc']);
+Route::get('ccc', [\App\Http\Controllers\Api\Admin\MarketController::class, 'ccc']);
 
 // Admin API Resource
 Route::post('/admin/login', [AppAuthController::class, 'login']);
