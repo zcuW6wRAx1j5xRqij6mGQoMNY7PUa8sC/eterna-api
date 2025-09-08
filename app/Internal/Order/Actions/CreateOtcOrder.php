3428fc5b551777220b72ffc1d30f96385339ef05
Cindy <cindy@netblaze.net>
2025-09-09 02:15:39 +0800

    opt

--- app/Internal/Order/Actions/CreateOtcOrder.php
+++ app/Internal/Order/Actions/CreateOtcOrder.php
@@ -11,53 +11,59 @@ use Illuminate\Support\Facades\DB;
 use InvalidArgumentException;
 use Illuminate\Http\Request;
 
-class CreateOtcOrder
-{
-
+class CreateOtcOrder {
+    
     public function __invoke(Request $request)
     {
         return DB::transaction(function () use ($request) {
-            $uid            = $request->user()->id;
-            $productId      = $request->get('product_id');
-            $quantity       = $request->get('quantity');
-            $paymentMethod  = $request->get('payment_method');
-            $comments       = $request->get('comments');
-            $tradeType      = $request->get('trade_type');
-
+            $uid           = $request->user()->id;
+            $productId     = $request->get('product_id');
+            $quantity      = $request->get('quantity');
+            $paymentMethod = $request->get('payment_method');
+            $comments      = $request->get('comments');
+            $tradeType     = $request->get('trade_type');
+            
             $quantity = parseNumber($quantity);
             if ($quantity <= 0) {
                 throw new InvalidArgumentException(__('Quantity is invalid'));
             }
             
             $product = OtcProduct::find($productId);
-            if(!$product){
+            if (!$product) {
                 throw new InvalidArgumentException(__('Product is not exist'));
             }
-            if($quantity<$product->min_limit || $quantity>$product->max_limit){
-                throw new InvalidArgumentException(__('Amount is out of range'));
+            
+            if ($tradeType == OrderEnums::TradeTypeBuy) {
+                if ($quantity < $product->min_limit || $quantity > $product->max_limit) {
+                    throw new InvalidArgumentException(__('Amount is out of range'));
+                }
+            } else {
+                if ($quantity < $product->sell_min_limit || $quantity > $product->sell_max_limit) {
+                    throw new InvalidArgumentException(__('Amount is out of range'));
+                }
             }
-
+            
             $price = $tradeType == OrderEnums::TradeTypeBuy ? $product->buy_price : $product->sell_price;
-
-            $order = new OtcOrder();
-            $order->uid             = $uid;
-            $order->product_id      = $productId;
-            $order->quantity        = $quantity;
-            $order->amount          = bcmul($quantity, $price, FundsEnums::DecimalPlaces);
-            $order->payment_method  = $paymentMethod;
-            $order->trade_type      = $tradeType;
-            $order->comments        = (string)$comments;
+            
+            $order                 = new OtcOrder();
+            $order->uid            = $uid;
+            $order->product_id     = $productId;
+            $order->quantity       = $quantity;
+            $order->amount         = bcmul($quantity, $price, FundsEnums::DecimalPlaces);
+            $order->payment_method = $paymentMethod;
+            $order->trade_type     = $tradeType;
+            $order->comments       = (string)$comments;
             if ($tradeType == OrderEnums::TradeTypeBuy) {
                 $order->buy_price = $price;
             } else {
                 $order->sell_price = $price;
             }
-            $order->status          = OrderEnums::TradeStatusPending;
+            $order->status = OrderEnums::TradeStatusPending;
             $order->save();
-
+            
             NewOtcOrder::dispatch($order);
             return true;
         });
     }
-
+    
 }
