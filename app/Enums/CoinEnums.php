3d01404615263efe0218b6dd0c89beec093f6b38
Cindy <cindy@netblaze.net>
2025-09-09 17:57:27 +0800

    opt

--- app/Enums/CoinEnums.php
+++ app/Enums/CoinEnums.php
@@ -3,17 +3,19 @@
 namespace App\Enums;
 
 class CoinEnums {
-
+    
     // 默认USDT 货币ID
     // const DefaultUSDTCoinID = 1;
-
+    
     // 默认USDC 货币ID
     const DefaultUSDTCoinID = 25;
-
+    
     const USDTTRC20 = 'USDT-TRC20';
     const USDTERC20 = 'USDT-ERC20';
     const USDTCoins = [
         self::USDTTRC20,
         self::USDTERC20,
     ];
+    
+    const COIN_ULX = 'ULX';
 }
