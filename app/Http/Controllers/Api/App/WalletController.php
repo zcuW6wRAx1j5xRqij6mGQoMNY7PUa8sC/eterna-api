92aaf2ce5fb1cc7e8c496bfd239d7a511f184a29
milo <milo>
2025-10-08 19:31:42 +0700

    add transfer list api

--- app/Http/Controllers/Api/App/WalletController.php
+++ app/Http/Controllers/Api/App/WalletController.php
@@ -15,9 +15,10 @@ use App\Http\Resources\UserWithdrawCollection;
 use App\Models\PlatformWallet;
 use App\Models\UserDeposit;
 use App\Models\UserWalletAddress;
+use App\Models\UserWalletSpotFlow;
 use App\Models\UserWithdraw;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
@@ -36,85 +37,82 @@ use Internal\Wallet\Actions\WalletSpotFlowList;
 use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
-/** @package App\Http\Controllers\Api\App */
-class WalletController extends ApiController {
-    
+class WalletController extends ApiController
+{
     public function summary(Request $request, Summary $summary)
     {
         return $this->ok($summary($request->user()));
     }
-    
+
     /**
      * 现货钱包
      *
-     * @param Request    $request
-     * @param SpotWallet $spotWallet
      *
      * @return JsonResponse
+     *
      * @throws InvalidArgumentException
      * @throws BindingResolutionException
      */
     public function spotWallet(Request $request, SpotWallet $spotWallet)
     {
         $isOtc = $request->query('is_otc', 0);
-        return $this->ok($spotWallet($request->user(),$isOtc));
+
+        return $this->ok($spotWallet($request->user(), $isOtc));
     }
-    
+
     /**
      * 现货钱包选择器
      *
-     * @param Request $request
      *
      * @return JsonResponse
+     *
      * @throws BindingResolutionException
      */
     public function spotWalletSelector(Request $request)
     {
-        return $this->ok((new SpotWallet())->selector($request->user()));
+        return $this->ok((new SpotWallet)->selector($request->user()));
     }
-    
+
     /**
      * 合约钱包
      *
-     * @param Request          $request
-     * @param DerivativeWallet $derivativeWallet
-     *
+     * @param  DerivativeWallet  $derivativeWallet
      * @return JsonResponse
+     *
      * @throws BindingResolutionException
      */
     public function futuresWallet(Request $request, FuturesWallet $futuresWallet)
     {
         return $this->ok($futuresWallet($request->user()));
     }
-    
+
     /**
      * 现货钱包流水
      *
-     * @param Request        $request
-     * @param SpotWalletFlow $spotWalletFlow
-     *
+     * @param  SpotWalletFlow  $spotWalletFlow
      * @return JsonResponse
+     *
      * @throws BadRequestException
      * @throws BindingResolutionException
      */
     public function spotWalletFlow(Request $request, WalletSpotFlowList $walletSpotFlowList)
     {
         $request->validate([
-            'page'      => 'numeric',
+            'page' => 'numeric',
             'page_size' => 'numeric',
             'flow_type' => ['string', Rule::in(SpotWalletFlowEnums::Maps)],
-            'coin_id'   => 'numeric',
+            'coin_id' => 'numeric',
         ]);
+
         return $this->ok($walletSpotFlowList($request, $request->user()));
     }
-    
+
     /**
      * 合约钱包流水
      *
-     * @param Request              $request
-     * @param DerivativeWalletFlow $derivativeWalletFlow
-     *
+     * @param  DerivativeWalletFlow  $derivativeWalletFlow
      * @return JsonResponse
+     *
      * @throws BadRequestException
      * @throws InvalidArgumentException
      * @throws BindingResolutionException
@@ -122,59 +120,82 @@ class WalletController extends ApiController {
     public function futuresWalletFlow(Request $request, WalletFuturesFlow $walletFuturesFlow)
     {
         $request->validate([
-            'page'      => 'numeric',
+            'page' => 'numeric',
             'page_size' => 'numeric',
             'flow_type' => ['string', Rule::in(WalletFuturesFlowEnums::Maps)],
         ]);
+
         return $this->ok($walletFuturesFlow($request, $request->user()));
-        
+
     }
-    
-    
+
     /**
      * 获取准许划转到现货的可用余额
      *
-     * @param Request                  $request
-     * @param FetchAvaiableChangeMoney $fetchAvaiableChangeMoney
      *
      * @return JsonResponse
+     *
      * @throws InvalidArgumentException
      * @throws BindingResolutionException
      */
     public function allowTransferSpotMoney(Request $request, FetchAvaiableChangeMoney $fetchAvaiableChangeMoney)
     {
         $money = $fetchAvaiableChangeMoney($request->user());
+
         return $this->ok($money);
     }
-    
+
+    /**
+     * 划转记录
+     *
+     * @return JsonResponse
+     *
+     * @throws BadRequestException
+     * @throws BindingResolutionException
+     */
+    public function transferList(Request $request)
+    {
+        $request->validate([
+            'page' => 'numeric',
+            'page_size' => 'numeric',
+        ]);
+
+        $data = UserWalletSpotFlow::where('uid', $request->user()->id)
+            ->whereIn('flow_type', [SpotWalletFlowEnums::FlowTypeTransferIn, SpotWalletFlowEnums::FlowTypeTransferOut])
+            ->orderByDesc('created_at')
+            ->paginate($request->get('page_size', 15));
+
+        return $this->ok($data);
+    }
+
     /**
      * 划转
      *
-     * @param Request  $request
-     * @param Transfer $transfer
      *
      * @return JsonResponse
+     *
      * @throws BindingResolutionException
      */
     public function transfer(Request $request, Transfer $transfer)
     {
         $request->validate([
-            'to'     => ['required', Rule::in([TransferEnums::WalletDerivative, TransferEnums::WalletSpot])],
+            'to' => ['required', Rule::in([TransferEnums::WalletDerivative, TransferEnums::WalletSpot])],
             'amount' => 'required|string',
         ]);
-        
+
         $request->user()->checkFundsLock();
-        
+
         $transfer($request, $request->user());
+
         return $this->ok(true);
     }
-    
+
     /**
      * 支持入金的货币
      *
-     * @param Request $request
      *
      * @return JsonResponse
+     *
      * @throws BindingResolutionException
      */
     public function supportDepositCoins(Request $request)
@@ -186,64 +207,67 @@ class WalletController extends ApiController {
         $coins = $coins->filter(function ($item) {
             return $item->platform && $item->platform->spot_deposit == CommonEnums::Yes;
         });
+
         return $this->ok(UserWalletAddressResource::collection(
             $coins
         ));
     }
-    
+
     /**
      * 支持提现的接口
      *
-     * @param Request $request
      *
      * @return void
+     *
      * @throws BindingResolutionException
      */
     public function supportWithdrawCoins(Request $request)
     {
         $data = PlatformWalletResource::collection(PlatformWallet::with(['coin'])->where('spot_withdraw', CommonEnums::Yes)->get());
+
         return $this->ok($data);
     }
-    
+
     /**
      * 入金
      *
-     * @param Request       $request
-     * @param SubmitDeposit $submitDeposit
      *
      * @return JsonResponse
+     *
      * @throws BindingResolutionException
      */
     public function deposit(Request $request, SubmitDeposit $submitDeposit)
     {
         $request->validate([
-            'amount'  => 'required|string',
+            'amount' => 'required|string',
             'coin_id' => 'required|numeric',
         ]);
-        //$submitDeposit($request);
+
+        // $submitDeposit($request);
         return $this->ok(true);
     }
-    
+
     /**
      * 充值列表
      *
-     * @param Request $request
      *
      * @return JsonResponse
+     *
      * @throws BadRequestException
      * @throws BindingResolutionException
      */
     public function depositList(Request $request)
     {
         $request->validate([
-            'page'      => 'numeric',
+            'page' => 'numeric',
             'page_size' => 'numeric',
         ]);
         $query = UserDeposit::where('uid', $request->user()->id);
-        $data  = $query->orderByDesc('created_at')->paginate($request->get('page_size', 15));
+        $data = $query->orderByDesc('created_at')->paginate($request->get('page_size', 15));
+
         return $this->ok(new UserDepositCollection($data));
     }
-    
+
     /*
      * 提现
      * @param Request $request
@@ -254,42 +278,44 @@ class WalletController extends ApiController {
     public function withdraw(Request $request, SubmitWithdraw $submitWithdraw)
     {
         $request->validate([
-            'coin_id'        => 'required|numeric',
-            'amount'         => 'required|string',
+            'coin_id' => 'required|numeric',
+            'amount' => 'required|string',
             'wallet_address' => 'required|string',
-            'child_name'     => 'required|string',
-            'trade_pwd'      => 'required|string',
+            'child_name' => 'required|string',
+            'trade_pwd' => 'required|string',
         ]);
         $tradePwd = $request->get('trade_pwd');
-        
+
         $request->user()->checkFundsLock();
-        
+
         $user = $request->user();
-        if (!$user->trade_password) {
+        if (! $user->trade_password) {
             throw new LogicException(__('Please set the transaction password first'), LogicException::NoSetTradePassword);
         }
-        if (!Hash::check($tradePwd, $user->trade_password)) {
+        if (! Hash::check($tradePwd, $user->trade_password)) {
             throw new LogicException(__('Incorrect transaction password'));
         }
+
         return $this->ok($submitWithdraw($request));
     }
-    
+
     /**
      * 申请提现列表
      *
-     * @param Request $request
      *
      * @return JsonResponse
+     *
      * @throws BadRequestException
      * @throws BindingResolutionException
      */
     public function withdrawList(Request $request)
     {
         $request->validate([
-            'page'      => 'numeric',
+            'page' => 'numeric',
             'page_size' => 'numeric',
         ]);
         $query = UserWithdraw::where('uid', $request->user()->id);
+
         return $this->ok(new UserWithdrawCollection($query->orderByDesc('created_at')->paginate($request->get('page_size', 15))));
     }
 }
--- app/Internal/Wallet/Actions/Transfer.php
+++ app/Internal/Wallet/Actions/Transfer.php
@@ -18,15 +18,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Internal\Order\Actions\FetchAvaiableChangeMoney;
 
-class Transfer {
-
+class Transfer
+{
     public function __invoke(Request $request, User $user)
     {
-        return DB::transaction(function() use($request, $user){
+        return DB::transaction(function () use ($request, $user) {
             $toWallet = $request->get('to');
-            $amount = parseNumber($request->get('amount',0));
+            $amount = parseNumber($request->get('amount', 0));
 
-            if (!is_numeric($amount)) {
+            if (! is_numeric($amount)) {
                 throw new LogicException(__('Whoops! Something went wrong'));
             }
             $amount = abs($amount);
@@ -34,48 +34,49 @@ class Transfer {
                 throw new LogicException(__('The amount is incorrect'));
             }
 
-            $spotWallet = UserWalletSpot::where('uid', $user->id)->where('coin_id',CoinEnums::DefaultUSDTCoinID)->lockForUpdate()->first();
-            if (!$spotWallet) {
+            $spotWallet = UserWalletSpot::where('uid', $user->id)->where('coin_id', CoinEnums::DefaultUSDTCoinID)->lockForUpdate()->first();
+            if (! $spotWallet) {
                 throw new LogicException(__('Insufficient account balance'));
             }
             $derivativeWallet = UserWalletFutures::where('uid', $user->id)->first();
-            if (!$derivativeWallet) {
-                $derivativeWallet = new UserWalletFutures();
+            if (! $derivativeWallet) {
+                $derivativeWallet = new UserWalletFutures;
                 $derivativeWallet->uid = $user->id;
                 $derivativeWallet->save();
                 $derivativeWallet = UserWalletFutures::where('uid', $user->id)->first();
             }
 
-            switch($toWallet) {
+            switch ($toWallet) {
                 case TransferEnums::WalletSpot:
                     $this->toSpotWallet($user, $derivativeWallet, $spotWallet, $amount);
-                break;
+                    break;
                 case TransferEnums::WalletDerivative:
                     $this->toDerivativeWallet($user, $derivativeWallet, $spotWallet, $amount);
-                break;
+                    break;
                 default:
                     throw new LogicException(__('Whoops! Something went wrong'));
             }
+
             return true;
         });
     }
 
-    public function toSpotWallet(User $user,UserWalletFutures $userDerivativelWallet,UserWalletSpot $userSpotWalletCoin, $amount) {
+    public function toSpotWallet(User $user, UserWalletFutures $userDerivativelWallet, UserWalletSpot $userSpotWalletCoin, $amount)
+    {
         $allowMoney = (new FetchAvaiableChangeMoney)($user);
         if (bcsub($allowMoney, $amount, FundsEnums::DecimalPlaces) < 0) {
             throw new LogicException(__('Insufficient account balance'));
         }
 
-
         $beforeFrom = $userDerivativelWallet->balance;
-        $d = bcsub($userDerivativelWallet->balance , $amount, FundsEnums::DecimalPlaces);
+        $d = bcsub($userDerivativelWallet->balance, $amount, FundsEnums::DecimalPlaces);
         if ($d < 0) {
             throw new LogicException(__('Insufficient account balance'));
         }
         $userDerivativelWallet->balance = $d;
         $userDerivativelWallet->save();
 
-        $flow = new UserWalletFuturesFlow();
+        $flow = new UserWalletFuturesFlow;
         $flow->uid = $user->id;
         $flow->flow_type = WalletFuturesFlowEnums::FlowTransferOut;
         $flow->before_amount = $beforeFrom;
@@ -83,13 +84,12 @@ class Transfer {
         $flow->after_amount = $userDerivativelWallet->balance;
         $flow->save();
 
-
-        $before =$userSpotWalletCoin->amount;
-        $userSpotWalletCoin->amount = bcadd($userSpotWalletCoin->amount , $amount, FundsEnums::DecimalPlaces);
+        $before = $userSpotWalletCoin->amount;
+        $userSpotWalletCoin->amount = bcadd($userSpotWalletCoin->amount, $amount, FundsEnums::DecimalPlaces);
         $userSpotWalletCoin->save();
 
-        $flow = new UserWalletSpotFlow();
-        $flow->uid =$user->id;
+        $flow = new UserWalletSpotFlow;
+        $flow->uid = $user->id;
         $flow->coin_id = $userSpotWalletCoin->coin_id;
         $flow->flow_type = SpotWalletFlowEnums::FlowTypeTransferIn;
         $flow->before_amount = $before;
@@ -97,11 +97,13 @@ class Transfer {
         $flow->after_amount = $userSpotWalletCoin->amount;
         $flow->save();
 
-        UserFuturesBalanceUpdate::dispatch($user->id,floatTransferString($userDerivativelWallet->balance));
+        UserFuturesBalanceUpdate::dispatch($user->id, floatTransferString($userDerivativelWallet->balance));
+
         return true;
     }
 
-    public function toDerivativeWallet(User $user, UserWalletFutures $userDerivativelWallet, UserWalletSpot $userSpotWalletCoin, $amount) {
+    public function toDerivativeWallet(User $user, UserWalletFutures $userDerivativelWallet, UserWalletSpot $userSpotWalletCoin, $amount)
+    {
         $beforeFrom = $userSpotWalletCoin->amount;
         $d = bcsub($userSpotWalletCoin->amount, $amount, FundsEnums::DecimalPlaces);
         if ($d < 0) {
@@ -110,8 +112,8 @@ class Transfer {
         $userSpotWalletCoin->amount = $d;
         $userSpotWalletCoin->save();
 
-        $flow = new UserWalletSpotFlow();
-        $flow->uid =$user->id;
+        $flow = new UserWalletSpotFlow;
+        $flow->uid = $user->id;
         $flow->coin_id = $userSpotWalletCoin->coin_id;
         $flow->flow_type = SpotWalletFlowEnums::FlowTypeTransferOut;
         $flow->before_amount = $beforeFrom;
@@ -119,12 +121,11 @@ class Transfer {
         $flow->after_amount = $userSpotWalletCoin->amount;
         $flow->save();
 
-
         $before = $userDerivativelWallet->balance;
-        $userDerivativelWallet->balance = bcadd($userDerivativelWallet->balance , $amount, FundsEnums::DecimalPlaces);
+        $userDerivativelWallet->balance = bcadd($userDerivativelWallet->balance, $amount, FundsEnums::DecimalPlaces);
         $userDerivativelWallet->save();
 
-        $flow = new UserWalletFuturesFlow();
+        $flow = new UserWalletFuturesFlow;
         $flow->uid = $user->id;
         $flow->flow_type = WalletFuturesFlowEnums::FlowTransferIn;
         $flow->before_amount = $before;
@@ -132,9 +133,8 @@ class Transfer {
         $flow->after_amount = $userDerivativelWallet->balance;
         $flow->save();
 
-        UserFuturesBalanceUpdate::dispatch($user->id,floatTransferString($userDerivativelWallet->balance));
+        UserFuturesBalanceUpdate::dispatch($user->id, floatTransferString($userDerivativelWallet->balance));
+
         return true;
     }
-
 }
-
